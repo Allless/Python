f60cc3cbd6ceb4b8ed10b40eb2dcdbe52a8e9f7d
hovoS <the.frost000@gmail.com>
2021-10-28 18:05:59 +0400

    fixes

--- Homework #4/findPattern.py
+++ Homework #4/findPattern.py
@@ -1,5 +1,5 @@
 def getPattern(word):
-    return [[x for x in range(len(word)) if word[x] == letter] for letter in word]
+    return set(tuple(x for x in range(len(word)) if word[x] == letter) for letter in set(word))
 
 def findAndReplacePattern(words, pattern):
     return [word for word in words if getPattern(word) == getPattern(pattern)]

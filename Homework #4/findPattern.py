6fdea3d05a0ed71da96175517df04940e9fb85ef
Hovhannes Sergoyan <91256813+Allless@users.noreply.github.com>
2021-11-03 19:33:34 +0400

    Update findPattern.py

--- Homework #4/findPattern.py
+++ Homework #4/findPattern.py
@@ -2,4 +2,5 @@ def getPattern(word):
     return [[x for x in range(len(word)) if word[x] == letter] for letter in word]
 
 def findAndReplacePattern(words, pattern):
-    return [word for word in words if getPattern(word) == getPattern(pattern)]
+    myPattern = getPattern(pattern)
+    return [word for word in words if getPattern(word) == myPattern]

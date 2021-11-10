af7d220649ef630469f2801bb73f2fa34010e8d6
hovoS <the.frost000@gmail.com>
2021-11-10 18:55:53 +0400

    fixes

--- Homework #4/UniqeEmailAdresses.py
+++ Homework #4/UniqeEmailAdresses.py
@@ -6,5 +6,5 @@ def getRealMail(email):
         adress = adress[0:adress.find('+')]
     return adress + domain
 
-def getUniqEmails(emails):
+def getUniqEmails(*emails):
     return len(set((getRealMail(mail) for mail in emails))) 
